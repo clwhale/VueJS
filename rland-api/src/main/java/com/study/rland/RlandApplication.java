4b93bc74f2bb57743bbafa58d41f0da682e5d49c
Jungwoo <cloudwhale99@gmail.com>
2023-01-13 23:44:08 +0900

    Add: gradle project
    Create a gradle project

---
+++ rland-api/src/main/java/com/study/rland/RlandApplication.java
@@ -0,0 +1,13 @@
+package com.study.rland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlandApplication.class, args);
+	}
+
+}
