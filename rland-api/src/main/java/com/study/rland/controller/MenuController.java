492bd95a0aac48431b252a2e5bd73bcc3f3271cf
Jungwoo <cloudwhale99@gmail.com>
2023-01-16 17:22:35 +0900

    ResponseEntity 데이터를 정의하고 처리하기
    매핑된 메소드들의 HTTP요청(Request) 또는 응답(Response)에 데이터를 어떻게 처리할지 배웠다.

--- rland-api/src/main/java/com/study/rland/controller/MenuController.java
+++ rland-api/src/main/java/com/study/rland/controller/MenuController.java
@@ -1,18 +1,28 @@
 package com.study.rland.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.study.rland.entity.Menu;
+
+import jakarta.validation.Valid;
+
 /**
  * 주로 view를 반환하기 위해 사용했던 @Controller와 달리
  * Data를 반환하기 위해 @ResponseBody를 활용하는 데
+ * 
  * @RestController는 @Controller에 @ResponseBody가 추가된 것으로
- * HTTP응답데이터(body)에 자바 객체가 매핑되어 전달 된다.
+ *                  HTTP응답데이터(body)에 자바 객체가 매핑되어 전달 된다.
  */
 @RestController
 @RequestMapping("menus")
@@ -21,31 +31,80 @@ public class MenuController {
     // EX: /menus?p=1&s=15
     @GetMapping
     public String getList(
-        @RequestParam(name = "p", defaultValue = "1") int page,
-        @RequestParam(name = "s", defaultValue = "15") int size) {
+            @RequestParam(name = "p", defaultValue = "1") int page,
+            @RequestParam(name = "s", defaultValue = "15") int size) {
         return String.format("get Menu List:page=%d, size%d", page, size);
     }
 
-    // EX: /menus/2
-    @GetMapping("{id}")
-    public String get(int id) {
-        return "get Menu";
+    /**
+     * EX: /menus/2
+     * 
+     * @param id
+     * @return
+     *         MediaType을 사용해서 들어오거나 나가는 데이터 속성을 강제함으로써 오류를 줄일 수 있다.
+     *         produces는 반환하는 데이터 타입을 정의한다.
+     */
+    @GetMapping(path = "{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE
+    })
+    /**
+     * ResponseEntity는 HTTP요청(Request) 또는 응답(Response)에 해당하는 Header와 Body를 포함하는
+     * 클래스이다.
+     * 
+     * @param id
+     * @return
+     */
+    public ResponseEntity<Menu> get(@PathVariable int id) {
+
+        Menu menu = Menu
+                .builder()
+                .name("아이스 카페라떼")
+                .price(6000)
+                .build();
+
+        return new ResponseEntity<Menu>(HttpStatus.OK);
     }
 
-    @PostMapping
-    public String add() {
-        return "add Menu";
+    /**
+     * consumes는 들어오는 데이터 타입을 정의할때 이용한다.
+     * 
+     * @param menu
+     * @return
+     */
+    @PostMapping(produces = {}, consumes = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE
+    })
+    public Menu create(@Valid @RequestBody Menu menu) {
+
+        System.out.println(menu);
+
+        /**
+         * db에 저장하고
+         * 방금 저장한 데이터를 다시 가져와서
+         */
+
+        return menu;
     }
-    
+
+    // Json이 기본이라 consume을 설정하지 않음
     @PutMapping
-    public String update() {
-        return "update Menu";
+    public Menu update(@RequestBody Menu menu) {
+
+        // error
+        Menu m = null;
+        m.getId();
+
+        return menu;
     }
-    
-    @DeleteMapping
-    public String delete() {
-        return "delete Menu";
+
+    // 삭제 메소드는 반환할게 없음
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        return ResponseEntity.noContent().build();
     }
+
     @GetMapping("hello")
     public String hello() {
         return "hello";
---
+++ rland-api/src/main/java/com/study/rland/entity/Menu.java
@@ -0,0 +1,68 @@
+package com.study.rland.entity;
+
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+// @Builder
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @ToString
+// @Getter
+public class Menu {
+    private int id;
+    @NotNull(message = "name은 null값일 수 없습니다.")
+    private String name;
+    private int price;
+
+    public Menu() {
+    }
+
+    public Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    @Builder
+    public Menu(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu [id=" + id + ", name=" + name + ", price=" + price + "]";
+    }
+
+}
