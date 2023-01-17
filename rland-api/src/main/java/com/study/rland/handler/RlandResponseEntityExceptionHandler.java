7b4b47d58f98414b8b817f6dedfc4989eb8fcf4a
Jungwoo <cloudwhale99@gmail.com>
2023-01-17 11:18:09 +0900

    예외 처리
    예외처리 방법과 h2 console 사용

---
+++ rland-api/src/main/java/com/study/rland/handler/RlandResponseEntityExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.study.rland.handler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class RlandResponseEntityExceptionHandler {
+        
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handlerAnyException(Exception ex, WebRequest request) {
+
+        return new ResponseEntity<>("any", new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
