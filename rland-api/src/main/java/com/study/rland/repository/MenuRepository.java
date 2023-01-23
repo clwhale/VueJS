1efbad123011d8cc7a81b148dc0ca52416e83502
Jungwoo <cloudwhale99@gmail.com>
2023-01-23 13:00:20 +0900

    Jpa 원상 복구

--- rland-api/src/main/java/com/study/rland/repository/MenuRepository.java
+++ rland-api/src/main/java/com/study/rland/repository/MenuRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.study.rland.entity.Menu;
 
-public interface MenuRepository { //extends JpaRepository<Menu, Integer> {
-    List<Menu> getList(int offset, int size);
-    Menu get(int id);
-    Menu create(Menu menu);
-}
-// public interface MenuRepository extends JpaRepository<Menu, Integer> {
-    
+// public interface MenuRepository { //extends JpaRepository<Menu, Integer> {
+//     List<Menu> getList(int offset, int size);
+//     Menu get(int id);
+//     Menu create(Menu menu);
 // }
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
+    
+}
